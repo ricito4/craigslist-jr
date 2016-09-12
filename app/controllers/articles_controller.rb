6c1ab3e0c192a06c42b8a85761603ffabdd5cd3f
Richie Ito & Kiren Srinivasan <pair+ricito4.tuneguru@devbootcamp.com>
2016-09-12 15:42:05 -0400

    Finished converting craigslist jr from Sinatra to Rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,29 +1,54 @@
 class ArticlesController < ApplicationController
-  def index
-    @categories
-  end
 
   def new
-
+    @article= Article.new
+    @categories = Category.all
   end
 
   def create
-
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to action: "secret", url: @article.url
+    else
+      @errors = @article.errors.full_messages
+      render 'new'
+    end
   end
 
   def show
-
+    @article = Article.find(params[:id])
+    @category = Category.find(@article.category_id)
   end
 
   def edit
-
+    @article = Article.find_by(url: params[:url])
+    @categories = Category.all
   end
 
   def update
-
+    @article = Article.find_by(url: params[:url])
+    if @article.update_attributes(article_params)
+      redirect_to action: "secret", url: @article.url
+    else
+      @errors = @article.errors.full_messages
+      @categories = Category.all
+      render 'edit'
+    end
   end
 
   def destroy
+    @article = Article.find_by(url: params[:url])
+    @article.destroy
+    redirect_to categories_path
+  end
 
+  def secret
+    @article = Article.find_by(url: params[:url])
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :url, :price, :description, :email, :category_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'categories#index'
 
-  resources :articles
+  get '/articles/:url/created', to: "articles#secret"
+  get '/articles/:url/edit', to: "articles#edit"
+  put '/articles/:url/created', to: "articles#update"
+  patch '/articles/:url/created', to: "articles#update"
+  delete '/articles/:url/edit', to: "articles#destroy"
+
+  resources :articles, except: [:index, :edit, :update, :destroy]
   resources :categories
 end
